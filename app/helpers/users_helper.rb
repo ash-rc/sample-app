72aed4ece9d5d617a23228b3c4787200676f660a
Ashley Campo <ashley.r.campo@gmail.com>
2017-07-29 13:11:37 -0700

    Added some prettifying to the User page, adding Gravatar to profiles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, size: 80)
+    # Downcasing the user email isn't necessary since we did it before
+    # saving the user in the User model.
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
