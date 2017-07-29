f0239d6ceb016d98923f845739a1cd8264d559a1
Ashley Campo <ashley.r.campo@gmail.com>
2017-07-29 12:49:11 -0700

    Added debugger info to development pages Added users show action to view user pages Setup User as a resource

--- db/migrate/20170721200048_create_users.rb
+++ db/migrate/20170721200048_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    # I was lazy and didn't make a new app, cleanup first 
+    # I was lazy and didn't make a new app, cleanup first
     drop_table :users
     create_table :users do |t|
       t.string :name
